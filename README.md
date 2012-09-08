872dd7c893938f77272303a1eed1603aae4e7a32
myatbaz <maliyatbaz@gmail.com>
2012-09-08 20:06:59 +0300

    tokens -> hash

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 induction
 =========
 
-Non-parametric feature bayed Bayesian algorithms
+Non-parametric feature based Bayesian algorithms
+
+v0.1
+	-Read gzip, stdin and regular text files
+	-Creates a hash entry for every input token
+	-Converts the token data to id-data using hash corresponding values
+
+------FUTURE WORK------
+	-Memory/CPU time logs might be helpful
+	-Read the model paper and prepare the input file accordingly
+	-Implement the model
+	-Implement the score functions
--- src/data.cpp
+++ src/data.cpp
@@ -7,7 +7,10 @@
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
-#include <boost/unordered_set.hpp>
+#include <boost/unordered/unordered_map.hpp>
+
+typedef boost::unordered_map<std::string, int> maps2i;
+#define DEBUG 1
 
 using std::cout;
 using std::cin;
@@ -17,70 +20,59 @@ using std::string;
 
 class data{
 public:
-  data(); //Construct empty data object
-  data(std::istream&); //Constructs one by reading a stream
-  void read(char *); //Constructs one by reading a stream
-  void info();
-  //  string get_inputSource() const {return inputSource;}  
-  int get_rows() const {return rows;}
-  int get_cols() const {return cols;}
-  
+     data(); //Construct empty data object
+     data(std::istream&); //Constructs one by reading a stream
+     void read(char *); //Constructs one by reading a stream
+     void info();
 private:
-  std::istream* inputSource;
-  int rows;
-  int cols;
-  int debug;
+     std::istream* inputSource;
+     std::vector<std::vector<int> > instanceVector;
+     maps2i hash;
 };
 
 /*method definitions*/
 
-data::data(): inputSource(&cin), rows(0), cols(0), debug(0) {};
-//data::data(std::istream& is):inputSource(&is),rows(0), cols(0), debug(0){};
-void data::read(char * fname)
-{
-  std::string check = fname == NULL ? "" : fname;
-  std::ifstream file;
-  boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
-  if (check.substr(check.find_last_of('.') + 1) == "gz"){
-    using namespace boost::iostreams;
-    file.open(fname, std::ios_base::in | std::ios_base::binary);
-    in.push(gzip_decompressor());
-    in.push(file);
-    inputSource = new std::istream(&in);
-  }
-  else if(not check.empty()){
-    file.open(fname);
-    if(file.good())     inputSource = &file;
-    else throw std::invalid_argument("File not found" );   
-  }
-  string line;
-  boost::unordered_map<std::string, int> map;
-  while(getline(*inputSource, line)){
-    if (debug > 10)cout << line << endl;
-    std::istringstream iss(line);
-    while(iss){ 
-      string subs; 
-      iss >> subs;
-      //      cout << subs << "["<< id <<"] ";
-    }
-    cout << endl;
-    rows++;
-  }    
-}
-
-void data::info()
-{
-  cout << "r:" << get_rows() << " c" << get_cols() << endl;  
-}
+data::data(): inputSource(&cin) {};
+void data::read(char * fname){
+     std::string check = fname == NULL ? "" : fname;
+     std::ifstream file;
+     boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
+     if (check.substr(check.find_last_of('.') + 1) == "gz"){
+          using namespace boost::iostreams;
+          file.open(fname, std::ios_base::in | std::ios_base::binary);
+          in.push(gzip_decompressor());
+          in.push(file);
+          inputSource = new std::istream(&in);
+     }
+     else if(not check.empty()){
+          file.open(fname);
+          if(file.good())     inputSource = &file;
+          else throw std::invalid_argument("File not found" );   
+     }
+     string line, subs;
+     int i = 0;
+     while(getline(*inputSource, line)){
+          if (line.empty()) continue;
+          std::istringstream iss(line);
+          if(DEBUG > 0 && i++ % 1000000 == 0) std::cerr << ".";
+          std::vector<int> instance;
+          while(iss >> subs){
+               int id = hash.size();
+               hash[subs] = id;
+               instance.push_back(id);
+          }
+          instanceVector.push_back(instance);
+     }    
+};
+void data::info(){
+     cout << "Data:" << instanceVector.size() << endl;
+     if (instanceVector.size() > 0)      cout << "Token/row:" << instanceVector[0].size() << endl;
+};
 int main(int argc, char** argv){
-  data d;
-  d.read(argv[1]);
-  d.info();
-//   //  cout << argv[1] << endl;
-//   //  data d(argv[1]);
-//   //  d.info();
-//   using namespace std;
-//   using namespace boost::iostreams;
-//   using namespace std;
-   return 1;
- }
+     data d;
+     d.read(argv[1]);
+     d.info();
+     cout << "looping" << endl;
+     while(1);
+     return 0;
+}
